fc8d862cc2eebff9ae8e3624ac57b45e7e217b1a
Stephen <grimsey.stephen@gmail.com>
2024-04-25 04:03:20 -0400

    Added 'GUI' to the RockPaperScissor game     -Added main visual for player and computer rolls     -Added 3 buttons to select player roll     -Added a scoreing
    *I want to point out this page is a mess and I didn't expect to come
    back to this to create a gui for. Plenty of bugs (score doesn't update
    for the final roll I think.. sometimes) and probably something about
    clicking to fast...
    **Also did I mention the code is a mess?

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
+let drawScore = 0;
+const pScore = document.querySelector(".player-score");
+const cScore = document.querySelector(".computer-score");
+const pVisual = document.querySelector(".player");
+const cVisual = document.querySelector(".computer");
 
 
 function getComputerChoice() {
@@ -7,68 +12,109 @@ function getComputerChoice() {
     
     switch (roll){
         case 1:
+            cVisual.textContent = "R"
             return 'rock';
         case 2:
+            cVisual.textContent = "P"
             return 'paper';
         case 3:
+            cVisual.textContent = "S"
             return 'scissor';
     }
 }
 
-function getHumanChoice(){
-    let entry = prompt('Enter "Rock", "Paper", or "Scissor"').toLowerCase();
-
-    if (entry == 'rock' || entry == 'paper' || entry == 'scissor'){
-        return entry;
-    } else {
-        return "Invalid";
-    }
-}
-
-function playRound(){
+function playRound(hand){
     let comp = getComputerChoice();
-    let player = getHumanChoice();
-
+    let player = hand.toLowerCase();
+    
 
     if(player == 'rock'){
+        pVisual.textContent = "R"
         if(comp == 'paper'){
             computerScore++;
+            pVisual.style.background = "salmon";
+            cVisual.style.background = "lightgreen";
+
         } else if(comp =='scissor'){
             humanScore++;
+            pVisual.style.background = "lightgreen";
+            cVisual.style.background = "salmon";
         } else {
+            drawScore++
+            pVisual.style.background = "lightslategray";
+            cVisual.style.background = "lightslategray";
             console.log('draw');
         }
     } else if (player == 'paper') {
+        pVisual.textContent = "P"
         if(comp == 'paper'){
+            drawScore++
+            pVisual.style.background = "lightslategray";
+            cVisual.style.background = "lightslategray";
             console.log('draw');
         } else if(comp =='scissor'){
             computerScore++;
+            pVisual.style.background = "salmon";
+            cVisual.style.background = "lightgreen";
         } else {
             humanScore++;
+            pVisual.style.background = "lightgreen";
+            cVisual.style.background = "salmon";
         }
     } else {
+        pVisual.textContent = "S"
         if(comp == 'paper'){
             humanScore++;
+            pVisual.style.background = "lightgreen";
+            cVisual.style.background = "salmon";
         } else if(comp =='scissor'){
+            drawScore++
+            pVisual.style.background = "lightslategray";
+            cVisual.style.background = "lightslategray";
             console.log('draw');
         } else {
             computerScore++;
+            pVisual.style.background = "salmon";
+            cVisual.style.background = "lightgreen";
         }
     }
-
+    pScore.textContent = humanScore;
+    cScore.textContent = computerScore;
     console.log(computerScore, humanScore)
 }
 
-function playGame(){
-    for(let i = 0; i < 5; i++){
-        playRound();
-    }
-    if(computerScore > humanScore){
-        console.log('Computer won');
-    } else if(humanScore > computerScore){
-        console.log('You won!');
-    } else {
-        console.log('Draw');
+function playGame(hand){
+    console.clear();
+
+    playRound(hand)
+
+    if((humanScore + computerScore + drawScore) == 5){
+        if(humanScore > computerScore){
+            alert("You won!")
+        } else if(computerScore > humanScore) {
+            alert("You lost!")
+        } else {
+            alert("Draw!")
+        }
+        drawScore = 0;
+
+        pScore.textContent = 0;
+        humanScore = 0;
+        pVisual.style.background = "none";
+        pVisual.textContent = "X";
+            
+        cScore.textContent = 0;
+        computerScore = 0;
+        cVisual.style.background = "none";
+        cVisual.textContent = "X";
     }
 }
+
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach((el) =>{
+    el.addEventListener("click", () => {
+        playGame(el.textContent);
+    })
+})
 
